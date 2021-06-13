36ccef6ded48f8605a8c91612fe0a19345afe06e
taaha <taaha.ham@gmail.com>
2021-06-13 13:54:33 +0000

    Sync LeetCode submission - Shuffle the Array (java)

---
+++ problems/shuffle_the_array/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int res[] = new int[2*n];
+        for(int i = 0; i<2*n; i+=2) {
+            res[i]=nums[i/2];
+            res[i+1]=nums[(i/2)+n];
+        }
+        return res;
+    }
+}
